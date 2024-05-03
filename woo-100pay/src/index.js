736e33c00d1b53c1093c75d65456b2203153f4c7
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-05-03 16:07:14 +0100

    fixed return statement in javascript

--- woo-100pay/src/index.js
+++ woo-100pay/src/index.js
@@ -6,8 +6,8 @@ const settings = getSetting( 'pay100_data', {} )
 const label = settings.title
 
 const Content = () => {
-	// return decodeEntities( settings.description || '' )
-	return 'Pay for Products using our Crypto Payment Gateway' 
+	
+	return settings.description
 }
 
 const { createElement : E } = React
@@ -30,32 +30,4 @@ registerPaymentMethod( {
 		features: settings.supports,
 	}
 } )
-
-
-
-
-// const pay100_settings = window.wc.wcSettings.getSetting( 'pay100_data', {} )
-
-// const pay100_label = pay100_settings.title
-
-// const Pay100Content = () => {
-// 	return pay100_settings.description || 'Default Text'  
-// }
-
-// const Pay100Label = ( props ) => {
-// 	const { PaymentMethodLabel } = props.components
-// 	return React.createElement(PaymentMethodLabel , {text: pay100_label } )
-// }
-
-// window.wc.wcBlocksRegistry.registerPaymentMethod( {
-// 	name: "pay100",
-// 	label: React.createElement(Pay100Label),
-// 	content: React.createElement(Pay100Content),
-// 	edit: React.createElement(Pay100Content),
-// 	canMakePayment: () => true,
-// 	ariaLabel: pay100_label,
-// 	supports: {
-// 		features: pay100_settings.supports,
-// 	}
-// } )
 
